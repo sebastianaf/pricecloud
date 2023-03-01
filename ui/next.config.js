b0255ea38e7238a45521c0256301da33995bebb8
sebastianaf <sebastianafanador95@gmail.com>
2023-03-01 00:16:50 -0500

    Update next.config.js

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,15 +1,1 @@
-const withImages = require('next-images');
 
-const redirects = {
-  async redirects() {
-    return [
-      {
-        source: '/dashboard',
-        destination: '/dashboard',
-        permanent: true
-      }
-    ];
-  }
-};
-
-module.exports = withImages(redirects);
