5091623b2d71ec90d389e0f5d04b0d327bc2a99f
sebastianaf <sebastianafanador95@gmail.com>
2023-08-20 15:55:08 -0500

    Update validation-schema.ts

--- api-01/src/config/validation-schema.ts
+++ api-01/src/config/validation-schema.ts
@@ -3,7 +3,7 @@ import * as Joi from 'joi';
 export default Joi.object({
   API_JWT_SECRET: Joi.string().required(),
   API_JWT_EXPIRATION_TIME: Joi.string().min(2).max(3).required(),
-  DB_HOST: Joi.string().ip().required(),
+  DB_HOST: Joi.string().hostname().required(),
   DB_PORT: Joi.number().min(100).max(65535).required(),
   DB_NAME: Joi.string().required(),
   DB_USER: Joi.string().required(),
