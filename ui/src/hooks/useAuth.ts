f35ae6a0367bd107d9fe37724854011dadef8557
sebastianaf <sebastianafanador95@gmail.com>
2023-09-21 08:52:27 -0500

    Fix path

--- ui/src/hooks/useAuth.ts
+++ ui/src/hooks/useAuth.ts
@@ -3,11 +3,15 @@ import Router from 'next/router';
 import { Login } from '../models/FormStates';
 import { path } from '../helper/path';
 import { authlessAxios } from '../helper/authlessAxios';
+import { API_HOST } from '../helper/environment';
 
 const useAuth = () => {
   const login = async (data: Login) => {
     try {
-      const response = await authlessAxios.post(path.auth, data);
+      const response = await authlessAxios.post(
+        `${API_HOST}/${path.auth}`,
+        data
+      );
       const { token } = await response.data;
       window.localStorage.setItem('token', token);
       Router.push('/dashboard');
