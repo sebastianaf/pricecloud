2d664fb30ddd9b89464a2e78567525ade8db1b14
sebastianaf <sebastianafanador95@gmail.com>
2023-09-21 13:23:15 -0500

    Add modal

---
+++ ui/src/components/Modal.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+
+interface ModalProps {
+  title?: string;
+  buttonText?: string;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  title = `Notificación`,
+  buttonText = 'Cerrar'
+}) => {
+  const [open, setOpen] = useState(true);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>{title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Let Google help apps determine location. This means sending
+            anonymous location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            {buttonText}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default Modal;
