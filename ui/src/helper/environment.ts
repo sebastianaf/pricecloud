180ad92b64912695df52da99274ec4d1b9a7e96c
sebastianaf <sebastianafanador95@gmail.com>
2023-09-21 09:32:02 -0500

    Fix

--- ui/src/helper/environment.ts
+++ ui/src/helper/environment.ts
@@ -1,9 +1,2 @@
-export const UI_PORT =
-  process.env.ENV === 'prod'
-    ? process.env.UI_PORT
-    : process.env.NEXT_PUBLIC_UI_PORT;
-
-export const API_HOST =
-  process.env.ENV === 'prod'
-    ? process.env.API_HOST
-    : process.env.NEXT_PUBLIC_API_HOST;
+export const UI_PORT = process.env.NEXT_PUBLIC_UI_PORT;
+export const API_HOST = process.env.NEXT_PUBLIC_API_HOST;
