33c5744be3531dae679a00a64987d0b26029b865
sebastianaf <sebastianafanador95@gmail.com>
2023-11-20 20:11:38 -0500

    Updating document

--- api-02/src/socket.ts
+++ api-02/src/socket.ts
@@ -11,20 +11,7 @@ const initializeSocket = (io: SocketIO.Server) => {
   cmd = io.of('/cmd');
 
   cmd.on('connection', (socket) => {
-    config.logger.info('Client connected to cmd');
-
-    socket.on('db-setup', async (data) => {
-      console.log(`-------------------------------------`);
-
-      config.logger.info('Client requested db-setup');
-      await dbSetup();
-    });
-
-    socket.on('data-download', async (data) => {
-      io.emit(`log`, `Client data-download ${data}`);
-      config.logger.info('Client data-download');
-      await dataDownload();
-    });
+    socket.emit('log', 'Bienvenido a la consola de comandos de Pricecloud');
   });
 };
 
