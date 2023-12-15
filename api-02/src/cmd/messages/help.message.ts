1ffa16424b3717f6aee867229b7310f3f37f5eb2
sebastianaf <sebastianafanador95@gmail.com>
2023-12-15 01:46:40 -0500

    avoid scrape command

--- api-02/src/cmd/messages/help.message.ts
+++ api-02/src/cmd/messages/help.message.ts
@@ -3,5 +3,6 @@ db:setup      -> Inicializa la configuración del servidor de BD local de precio
 data:download -> Descargaun archivo con la data de precios desde la BD remota.
 data:dump     -> Genera un volcado de la data de precios en desde la BD local.
 data:load     -> Carga la data de precios a la BD local desde el archivo de volcado o descargado.
-data:scrape   -> Busca actualizaciones de precios desde los CCSPs.
 job:update    -> Ejecuta manualmente el proceso de actualización de la base de datos de precios.`;
+
+const unavailableCommands = `data:scrape   -> Busca actualizaciones de precios desde los CCSPs.`;
