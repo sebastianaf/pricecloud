a0d863d9cea17fdc316b6dad93a39dec58d43f70
sebastianaf <sebastianafanador95@gmail.com>
2024-02-07 16:49:03 -0500

    Update index.ts

--- api-01/src/docs/index.ts
+++ api-01/src/docs/index.ts
@@ -6,13 +6,8 @@ const mainReadme = readFileSync(
   'utf8',
 );
 
-
-const updateSKUDrawio = readFileSync(
-  join(__dirname, `update-sku.drawio.svg`),
-  'utf8',
-);
+const erDrawio = readFileSync(join(__dirname, `er.drawio.svg`), 'utf8');
 
 let mainDocs = mainReadme.replace(`erDrawio`, erDrawio);
-mainDocs = mainDocs.replace(`erDrawio`, updateSKUDrawio);
 
 export { mainDocs };
